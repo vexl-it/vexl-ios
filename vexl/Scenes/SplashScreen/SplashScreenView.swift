057a09c41634e1217278a0c8f4f686ee39c670be
Adam Salih <salih.max@gmail.com>
2022-02-15 22:58:34 +0100

    Add splash screen

--- vexl/Scenes/SplashScreen/SplashScreenView.swift
+++ vexl/Scenes/SplashScreen/SplashScreenView.swift
@@ -10,8 +10,41 @@ import SwiftUI
 struct SplashScreenView: View {
     @StateObject var viewModel: SplashScreenViewModel
 
+    let cleevioColor: Color = .init(red: 247 / 255, green: 207 / 255, blue: 88 / 255, opacity: 1)
+    let logoSize: CGFloat = 200
+
     var body: some View {
-        Text("SplashScreen")
+        ZStack {
+            ZStack {
+                ZStack {
+                    Circle()
+                        .frame(width: logoSize * 0.64, height: logoSize * 0.64)
+
+                    Circle()
+                        .frame(width: logoSize * 0.37, height: logoSize * 0.37)
+                        .foregroundColor(cleevioColor)
+
+                    Rectangle()
+                        .frame(width: logoSize * 0.64 * 0.5, height: logoSize * 0.64, alignment: .trailing)
+                        .foregroundColor(cleevioColor)
+                        .offset(x: logoSize * 0.64 * 0.25, y: 0)
+                }
+                .rotationEffect(.degrees(30))
+
+                VStack(spacing: 0) {
+                    Rectangle()
+                        .frame(height: logoSize * 0.64 - logoSize * 0.18)
+                        .foregroundColor(.clear)
+
+                    Circle()
+                        .frame(width: logoSize * 0.13, height: logoSize * 0.13)
+                        .offset(x: (logoSize * 0.13) / 2, y: 0)
+                }
+            }
+        }
+        .frame(width: logoSize, height: logoSize)
+        .background(cleevioColor)
+        .clipShape(RoundedRectangle(cornerRadius: logoSize * 0.4, style: .continuous))
     }
 }
 
