76d9c213ea40d66ed4a50250eb4c1fa7deb84709
Adam Salih <salih.max@gmail.com>
2022-02-15 23:03:57 +0100

    Update splash screen colors

--- vexl/Scenes/SplashScreen/SplashScreenView.swift
+++ vexl/Scenes/SplashScreen/SplashScreenView.swift
@@ -15,36 +15,45 @@ struct SplashScreenView: View {
 
     var body: some View {
         ZStack {
+            Rectangle()
+                .foregroundColor(cleevioColor)
+            logo
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+
+    var logo: some View {
             ZStack {
                 ZStack {
-                    Circle()
-                        .frame(width: logoSize * 0.64, height: logoSize * 0.64)
+                    ZStack {
+                        Circle()
+                            .frame(width: logoSize * 0.64, height: logoSize * 0.64)
+                            .foregroundColor(cleevioColor)
 
-                    Circle()
-                        .frame(width: logoSize * 0.37, height: logoSize * 0.37)
-                        .foregroundColor(cleevioColor)
+                        Circle()
+                            .frame(width: logoSize * 0.37, height: logoSize * 0.37)
 
-                    Rectangle()
-                        .frame(width: logoSize * 0.64 * 0.5, height: logoSize * 0.64, alignment: .trailing)
-                        .foregroundColor(cleevioColor)
-                        .offset(x: logoSize * 0.64 * 0.25, y: 0)
-                }
-                .rotationEffect(.degrees(30))
+                        Rectangle()
+                            .frame(width: logoSize * 0.64 * 0.5, height: logoSize * 0.64, alignment: .trailing)
+                            .offset(x: logoSize * 0.64 * 0.25, y: 0)
+                    }
+                    .rotationEffect(.degrees(30))
 
-                VStack(spacing: 0) {
-                    Rectangle()
-                        .frame(height: logoSize * 0.64 - logoSize * 0.18)
-                        .foregroundColor(.clear)
+                    VStack(spacing: 0) {
+                        Rectangle()
+                            .frame(height: logoSize * 0.64 - logoSize * 0.18)
+                            .foregroundColor(.clear)
 
-                    Circle()
-                        .frame(width: logoSize * 0.13, height: logoSize * 0.13)
-                        .offset(x: (logoSize * 0.13) / 2, y: 0)
+                        Circle()
+                            .frame(width: logoSize * 0.13, height: logoSize * 0.13)
+                            .offset(x: (logoSize * 0.13) / 2, y: 0)
+                            .foregroundColor(cleevioColor)
+                    }
                 }
             }
-        }
-        .frame(width: logoSize, height: logoSize)
-        .background(cleevioColor)
-        .clipShape(RoundedRectangle(cornerRadius: logoSize * 0.4, style: .continuous))
+            .frame(width: logoSize, height: logoSize)
+            .background(Color.black)
+            .clipShape(RoundedRectangle(cornerRadius: logoSize * 0.4, style: .continuous))
     }
 }
 
