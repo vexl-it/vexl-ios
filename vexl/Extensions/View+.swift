a0b24b20662d3325e7871e4675b21cda444068ae
Diego Espinoza <diego@Diegos-MacBook-Pro.local>
2022-02-25 09:46:33 -0500

    removed if modifier

--- vexl/Extensions/View+.swift
+++ vexl/Extensions/View+.swift
@@ -8,44 +8,6 @@
 import SwiftUI
 
 extension View {
-    @ViewBuilder func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-    
-    @ViewBuilder func `if`<TrueContent: View, FalseContent: View>(_ condition: Bool,
-                                                                  if ifTransform: (Self) -> TrueContent,
-                                                                  else elseTransform: (Self) -> FalseContent) -> some View {
-        if condition {
-            ifTransform(self)
-        } else {
-            elseTransform(self)
-        }
-    }
-
-    @ViewBuilder func ifLet<V, Transform: View>(_ value: V?, transform: (Self, V) -> Transform) -> some View {
-        if let value = value {
-            transform(self, value)
-        } else {
-            self
-        }
-    }
-
-    @ViewBuilder func ifLet<V, TrueContent: View, FalseContent: View>(_ value: V?,
-                                                                      if ifTransform: (Self, V) -> TrueContent,
-                                                                      else elseTransform: (Self) -> FalseContent) -> some View {
-        if let value = value {
-            ifTransform(self, value)
-        } else {
-            elseTransform(self)
-        }
-    }
-}
-
-extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape(ClippedRoundedCorners(radius: radius, corners: corners))
     }
