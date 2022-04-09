e31ff247ba8660cfd558a7520b60c21306000240
Diego Espinoza <diego@Diegos-MacBook-Pro.local>
2022-04-10 01:44:27 +0200

    removed file

---
+++ vexl/Views/Headers/CoinValueVariationHeader.swift
@@ -0,0 +1,22 @@
+//
+//  ValueVariationHeader.swift
+//  vexl
+//
+//  Created by Diego Espinoza on 10/04/22.
+//
+
+import SwiftUI
+
+struct CoinValueVariationHeader: View {
+    
+    var body: some View {
+        Text("123123")
+    }
+
+}
+
+struct CoinValueVariationHeaderPreview: PreviewProvider {
+    static var previews: some View {
+        CoinValueVariationHeader()
+    }
+}
