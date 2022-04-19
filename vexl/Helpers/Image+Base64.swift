4fdc6a8da417de5449106c74f60bbb9360707ae3
Diego Espinoza <diego@Diegos-MacBook-Pro.local>
2022-04-19 12:15:35 +0100

    removed self

--- vexl/Helpers/Image+Base64.swift
+++ vexl/Helpers/Image+Base64.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIImage {
     var base64: String? {
-        self.jpegData(compressionQuality: 1)?.base64EncodedString()
+        jpegData(compressionQuality: 1)?.base64EncodedString()
     }
 }
 
