2bce8d1f5b965d2121a14707a202b4c39437918a
Adam Salih <salih.max@gmail.com>
2022-04-22 17:17:18 +0200

    Use throw on unit tests functions

--- vexlTests/vexlTests.swift
+++ vexlTests/vexlTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 @testable import vexl
 
-// swiftlint:disable force_try
 class VexlTests: XCTestCase {
     let cryptoService: CryptoServiceType = CryptoService()
 
@@ -25,43 +24,36 @@ class VexlTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testSHA() {
-        XCTAssertNoThrow(try cryptoService.hashSHA(data: message))
-        let digestSHA = try! cryptoService.hashSHA(data: message)
+    func testSHA() throws {
+        let digestSHA = try cryptoService.hashSHA(data: message)
         XCTAssertEqual(digestSHA, "uwtXAF8BAYsZwnjFUnOmARj/3T5XkMzIpIytA5B/pSE=")
     }
 
-    func testHMAC() {
-        XCTAssertNoThrow(try cryptoService.hashHMAC(password: password, message: message))
-        let digest = try! cryptoService.hashHMAC(password: password, message: message)
+    func testHMAC() throws {
+        let digest = try cryptoService.hashHMAC(password: password, message: message)
         let verifiedHMAC = cryptoService.verifyHMAC(password: password, message: message, digest: digest)
         XCTAssertTrue(verifiedHMAC)
     }
 
-    func testAES() {
-        XCTAssertNoThrow(try cryptoService.encryptAES(password: password, secret: message))
-        let cipher = try! cryptoService.encryptAES(password: password, secret: message)
-        XCTAssertNoThrow(try cryptoService.decryptAES(password: password, cipher: cipher))
-        let decryptedMessage = try! cryptoService.decryptAES(password: password, cipher: cipher)
+    func testAES() throws {
+        let cipher = try cryptoService.encryptAES(password: password, secret: message)
+        let decryptedMessage = try cryptoService.decryptAES(password: password, cipher: cipher)
         XCTAssertEqual(decryptedMessage, message)
     }
 
-    func testECDSA() {
+    func testECDSA() throws {
         let keys = ECKeys()
         XCTAssertNotNil(keys.privateKey)
-        XCTAssertNoThrow(try cryptoService.signECDSA(keys: keys, message: message))
-        let signature = try! cryptoService.signECDSA(keys: keys, message: message)
+        let signature = try cryptoService.signECDSA(keys: keys, message: message)
         let verifiedECDSA = cryptoService.verifyECDSA(publicKey: keys.publicKey, message: message, signature: signature)
         XCTAssertTrue(verifiedECDSA)
     }
 
-    func testECIES() {
+    func testECIES() throws {
         let keys = ECKeys()
         XCTAssertNotNil(keys.privateKey)
-        XCTAssertNoThrow(try cryptoService.encryptECIES(publicKey: keys.publicKey, secret: message))
-        let cipher = try! cryptoService.encryptECIES(publicKey: keys.publicKey, secret: message)
-        XCTAssertNoThrow(try cryptoService.decryptECIES(keys: keys, cipher: cipher))
-        let decryptedMessage = try! cryptoService.decryptECIES(keys: keys, cipher: cipher)
+        let cipher = try cryptoService.encryptECIES(publicKey: keys.publicKey, secret: message)
+        let decryptedMessage = try cryptoService.decryptECIES(keys: keys, cipher: cipher)
         XCTAssertEqual(decryptedMessage, message)
     }
 }
