19a1e98adfb2cfef799f2dcaf576148d77d8e4b2
Diego Espinoza <diego@Diegos-MacBook-Pro.local>
2022-05-04 21:48:00 +0200

    cleaned warnings

--- vexl/Views/OptionPicker/PhoneNumberTextFieldView.swift
+++ vexl/Views/OptionPicker/PhoneNumberTextFieldView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 import PhoneNumberKit
 
 struct PhoneNumberTextFieldView: UIViewRepresentable {
-    
+
     let placeholder: String
     let font: UIFont
     @Binding var text: String
-    
+
     func updateUIView(_ uiView: PhoneNumberTextField, context: Context) {
     }
-    
+
     func makeUIView(context: Context) -> PhoneNumberTextField {
         let phoneNumberTextField = PhoneNumberTextField()
         phoneNumberTextField.addTarget(context.coordinator, action: #selector(Coordinator.onTextChange), for: .editingChanged)
@@ -26,14 +26,14 @@ struct PhoneNumberTextFieldView: UIViewRepresentable {
         phoneNumberTextField.withFlag = true
         return phoneNumberTextField
     }
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
     final class Coordinator: NSObject, UITextFieldDelegate {
         var parent: PhoneNumberTextFieldView
-        
+
         init(_ parent: PhoneNumberTextFieldView) {
             self.parent = parent
         }
