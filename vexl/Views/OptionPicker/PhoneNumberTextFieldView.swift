b8e82cbdc0fb70c1da55d6a54439b0fd81893ade
Lukáš Valenta <valenta@lvalenta.cz>
2022-05-04 21:31:52 +0200

    Cleared code

--- vexl/Views/OptionPicker/PhoneNumberTextFieldView.swift
+++ vexl/Views/OptionPicker/PhoneNumberTextFieldView.swift
@@ -15,30 +15,6 @@ struct PhoneNumberTextFieldView: UIViewRepresentable {
     @Binding var text: String
     
     func updateUIView(_ uiView: PhoneNumberTextField, context: Context) {
-        guard let newText = uiView.text else { return }
-        DispatchQueue.main.async {
-            if self.text != newText {
-                print(self.text, newText)
-                self.text = newText
-            }
-        }
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    final class Coordinator: NSObject, UITextFieldDelegate {
-        var parent: PhoneNumberTextFieldView
-        
-        init(_ parent: PhoneNumberTextFieldView) {
-            self.parent = parent
-        }
-        
-        @objc
-        func onTextChange(textField: UITextField) {
-            parent.text = textField.text!
-        }
     }
     
     func makeUIView(context: Context) -> PhoneNumberTextField {
@@ -46,5 +22,22 @@ struct PhoneNumberTextFieldView: UIViewRepresentable {
         phoneNumberTextField.addTarget(context.coordinator, action: #selector(Coordinator.onTextChange), for: .editingChanged)
         phoneNumberTextField.placeholder = placeholder
         return phoneNumberTextField
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    final class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: PhoneNumberTextFieldView
+        
+        init(_ parent: PhoneNumberTextFieldView) {
+            self.parent = parent
+        }
+
+        @objc
+        func onTextChange(textField: UITextField) {
+            parent.text = textField.text!
+        }
     }
 }
