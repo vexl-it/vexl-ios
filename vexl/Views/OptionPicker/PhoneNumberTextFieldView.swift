71cd240d332afd7a111dce484126a5ff37013c36
Lukáš Valenta <valenta@lvalenta.cz>
2022-05-04 21:27:20 +0200

    Rewritten with using Coordinator

--- vexl/Views/OptionPicker/PhoneNumberTextFieldView.swift
+++ vexl/Views/OptionPicker/PhoneNumberTextFieldView.swift
@@ -10,19 +10,40 @@ import SwiftUI
 import PhoneNumberKit
 
 struct PhoneNumberTextFieldView: UIViewRepresentable {
-
+    
     let placeholder: String
     @Binding var text: String
-
+    
     func updateUIView(_ uiView: PhoneNumberTextField, context: Context) {
+        guard let newText = uiView.text else { return }
         DispatchQueue.main.async {
-            guard let newText = uiView.text else { return }
-            text = newText
+            if self.text != newText {
+                print(self.text, newText)
+                self.text = newText
+            }
         }
     }
-
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: PhoneNumberTextFieldView
+        
+        init(_ parent: PhoneNumberTextFieldView) {
+            self.parent = parent
+        }
+        
+        @objc
+        func onTextChange(textField: UITextField) {
+            parent.text = textField.text!
+        }
+    }
+    
     func makeUIView(context: Context) -> PhoneNumberTextField {
         let phoneNumberTextField = PhoneNumberTextField()
+        phoneNumberTextField.addTarget(context.coordinator, action: #selector(Coordinator.onTextChange), for: .editingChanged)
         phoneNumberTextField.placeholder = placeholder
         return phoneNumberTextField
     }
