0e7b41863c3c30f7b48f2bf3ef2868b4bb2da14b
Diego Espinoza <diego@Diegos-MacBook-Pro.local>
2022-06-01 13:46:38 -0500

    added common friend view

---
+++ vexl/Scenes/ChatMessage/Components/ChatMessageCommonFriendsView.swift
@@ -0,0 +1,15 @@
+//
+//  ChatMessageCommonFriendsView.swift
+//  vexl
+//
+//  Created by Diego Espinoza on 1/06/22.
+//
+
+import SwiftUI
+
+struct ChatMessageCommonFriendsView: View {
+
+    var body: some View {
+        Text(L.chatMessageCommonFriend())
+    }
+}
