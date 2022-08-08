af3da148df25ada66ae9adcf33f63184565e318c
Diego <diego.espinoza@cleevio.com>
2022-08-08 14:59:35 -0500

    base layout for qr scan

--- vexl/Scenes/Groups/ScanQR/GroupsScanQRView.swift
+++ vexl/Scenes/Groups/ScanQR/GroupsScanQRView.swift
@@ -10,8 +10,43 @@ import SwiftUI
 struct GroupsScanQRView: View {
 
     @ObservedObject var viewModel: GroupsScanQRViewModel
-    
+
     var body: some View {
-        Text("123")
+        ZStack {
+            Text("Camera will go here")
+                .foregroundColor(.white)
+            
+            VStack {
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(R.image.profile.qrManual.name)
+                        
+                        Text("Enter code manually")
+                            .textStyle(.paragraphSmallSemiBold)
+                            .foregroundColor(Appearance.Colors.primaryText)
+                    }
+                }
+                .padding(Appearance.GridGuide.smallPadding)
+                .background(Appearance.Colors.whiteText)
+                .cornerRadius(Appearance.GridGuide.buttonCorner)
+                .padding(.bottom, Appearance.GridGuide.largePadding1)
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
 }
+
+#if DEBUG || DEVEL
+
+struct GroupsScanQRViewPreview: PreviewProvider {
+    static var previews: some View {
+        GroupsScanQRView(viewModel: .init())
+            .previewDevice("iPhone 11")
+    }
+}
+
+#endif
