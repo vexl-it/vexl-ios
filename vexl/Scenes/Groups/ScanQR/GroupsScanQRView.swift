4d8956b3e6ab430563ffec171afa38bb0efaafe0
Diego <diego.espinoza@cleevio.com>
2022-08-08 19:26:21 -0500

    added camera preview + scene

--- vexl/Scenes/Groups/ScanQR/GroupsScanQRView.swift
+++ vexl/Scenes/Groups/ScanQR/GroupsScanQRView.swift
@@ -13,16 +13,39 @@ struct GroupsScanQRView: View {
 
     var body: some View {
         ZStack {
-            Text("Camera will go here")
-                .foregroundColor(.white)
-            
+            if viewModel.isCameraAvailable && viewModel.showCamera {
+                Text("Should show camera")
+            } else if viewModel.isCameraAvailable && !viewModel.showCamera {
+                VStack {
+                    Text("Vexl has no access to the camera")
+                        .foregroundColor(Appearance.Colors.whiteText)
+                        .textStyle(.paragraphSemibold)
+                        .padding(.bottom, Appearance.GridGuide.padding)
+
+                    Text("Go to Settings and grant access to the camera functionality.")
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Appearance.Colors.gray3)
+                        .textStyle(.paragraphSmall)
+                        .padding(.horizontal, Appearance.GridGuide.largePadding2)
+                }
+            } else {
+                Button(L.continue()) {
+                    viewModel.action.send(.codeScan(code: viewModel.mockCode))
+                }
+                .textStyle(.paragraphSmallSemiBold)
+                .foregroundColor(Appearance.Colors.primaryText)
+                .padding(Appearance.GridGuide.smallPadding)
+                .background(Appearance.Colors.whiteText)
+                .cornerRadius(Appearance.GridGuide.buttonCorner)
+            }
+
             VStack {
                 Button {
-                    
+                    viewModel.action.send(.manualInputTap)
                 } label: {
                     HStack {
                         Image(R.image.profile.qrManual.name)
-                        
+
                         Text("Enter code manually")
                             .textStyle(.paragraphSmallSemiBold)
                             .foregroundColor(Appearance.Colors.primaryText)
--- vexl/Scenes/Groups/ScanQR/GroupsScanQRViewModel.swift
+++ vexl/Scenes/Groups/ScanQR/GroupsScanQRViewModel.swift
@@ -7,13 +7,28 @@
 
 import Foundation
 import Cleevio
+import AVFoundation
 
 final class GroupsScanQRViewModel: ViewModelType, ObservableObject {
+
+    // MARK: - Properties for simulator/mock testing
+
+    var mockCode = "111111"
+    var isCameraAvailable: Bool {
+        #if targetEnvironment(simulator)
+        return false
+        #else
+        return true
+        #endif
+    }
 
     // MARK: - Actions Bindings
 
     enum UserAction: Equatable {
         case dismissTap
+        case cameraAccessRequest
+        case codeScan(code: String)
+        case manualInputTap
     }
 
     let action: ActionSubject<UserAction> = .init()
@@ -21,11 +36,14 @@ final class GroupsScanQRViewModel: ViewModelType, ObservableObject {
     // MARK: - View Bindings
 
     @Published var primaryActivity: Activity = .init()
+    @Published var showCamera = false
 
     // MARK: - Coordinator Bindings
 
     enum Route: Equatable {
         case dismissTapped
+        case codeScanned(code: String)
+        case manualInputTapped
     }
 
     var route: CoordinatingSubject<Route> = .init()
@@ -41,5 +59,40 @@ final class GroupsScanQRViewModel: ViewModelType, ObservableObject {
     }
 
     private func setupActions() {
+        action
+            .filter { $0 == .cameraAccessRequest }
+            .withUnretained(self)
+            .sink { owner, _ in
+                owner.requestCameraAccess()
+            }
+            .store(in: cancelBag)
+
+        action
+            .compactMap { action -> String? in
+                if case let .codeScan(code) = action { return code }
+                return nil
+            }
+            .map { code -> Route in .codeScanned(code: code) }
+            .subscribe(route)
+            .store(in: cancelBag)
+
+        action
+            .filter { $0 == .manualInputTap }
+            .map { _ -> Route in .manualInputTapped }
+            .subscribe(route)
+            .store(in: cancelBag)
+    }
+
+    private func requestCameraAccess() {
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+        if cameraAuthorizationStatus == .authorized {
+            showCamera = true
+        } else {
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                DispatchQueue.main.sync {
+                    self?.showCamera = granted
+                }
+            }
+        }
     }
 }
