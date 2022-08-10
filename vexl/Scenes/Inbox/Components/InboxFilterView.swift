e5555e4d67b6a9f050aa084de359056c214ed00c
Diego <diego.espinoza@cleevio.com>
2022-08-10 12:09:21 -0500

    fixed coredata property name

--- vexl/Scenes/Inbox/Components/InboxFilterView.swift
+++ vexl/Scenes/Inbox/Components/InboxFilterView.swift
@@ -28,9 +28,9 @@ struct InboxFilterView: View {
         var chatPredicate: NSPredicate? {
             switch self {
             case .buy:
-                return NSPredicate(format: "receiverKeyPair.offer.offerTypeRawType == '\(OfferType.buy.rawValue)'")
+                return NSPredicate(format: "receiverKeyPair.userOffer.offerTypeRawType == '\(OfferType.buy.rawValue)'")
             case .sell:
-                return NSPredicate(format: "receiverKeyPair.offer.offerTypeRawType == '\(OfferType.sell.rawValue)'")
+                return NSPredicate(format: "receiverKeyPair.userO   ffer.offerTypeRawType == '\(OfferType.sell.rawValue)'")
             case .all:
                 return nil
             }
