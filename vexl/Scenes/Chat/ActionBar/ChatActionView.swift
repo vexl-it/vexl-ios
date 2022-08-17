12346651a5be49a69dfe98a51a9475ea46ddf8c8
Diego <diego.espinoza@cleevio.com>
2022-08-17 13:34:31 -0500

    updated condition

--- vexl/Scenes/Chat/ActionBar/ChatActionView.swift
+++ vexl/Scenes/Chat/ActionBar/ChatActionView.swift
@@ -16,7 +16,7 @@ struct ChatActionView: View {
     private var availableActions: [ChatActionOption] {
         var actions = ChatActionOption.allCases
 
-        if viewModel.showIdentityRequest {
+        if !viewModel.showIdentityRequest {
             actions = actions.filter { $0 != .revealIdentity }
         }
 
