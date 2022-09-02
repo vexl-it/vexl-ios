a606d36ca19e1bcfe35a5dbd6665d2bf224a1469
Adam Salih <salih.max@gmail.com>
2022-09-02 19:31:07 +0200

    Accept suggestion with diacritics

--- vexl/Services/Networking/MapyService.swift
+++ vexl/Services/Networking/MapyService.swift
@@ -14,12 +14,14 @@ protocol MapyServiceType {
 
 final class MapyService: BaseService, MapyServiceType {
     func getSuggestions(for text: String) -> AnyPublisher<[LocationSuggestion], Error> {
-        request(type: MapyResponse.self, endpoint: MapyRouter.suggestions(text: text))
+        let diacriticInsensitiveText = text.folding(options: .diacriticInsensitive, locale: nil).lowercased()
+        return request(type: MapyResponse.self, endpoint: MapyRouter.suggestions(text: text))
             .map(\.result)
             .map { $0.map { $0.userData } }
             .map {
-                Set($0.filter {
-                    !$0.city.isEmpty && $0.city.lowercased().contains(text.lowercased())
+                Set($0.filter { suggestion in
+                    let diacriticInsensitiveSuggestionCity = suggestion.city.folding(options: .diacriticInsensitive, locale: nil).lowercased()
+                    return !suggestion.city.isEmpty && diacriticInsensitiveSuggestionCity.lowercased().contains(diacriticInsensitiveText.lowercased())
                 })
             }
             .map(Array.init)
