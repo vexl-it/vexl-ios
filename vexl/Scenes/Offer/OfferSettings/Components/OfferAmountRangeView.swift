a3a54afd4613442381d4b1c4e146a3453e7e5028
Adam Salih <salih.max@gmail.com>
2022-09-13 23:15:52 +0200

    Implement OfferAmountTextField into offer settings

--- vexl/Scenes/Offer/OfferSettings/Components/OfferAmountRangeView.swift
+++ vexl/Scenes/Offer/OfferSettings/Components/OfferAmountRangeView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct OfferAmountRangeView: View {
     let currency: Currency
-    let currentValue: Binding<ClosedRange<Int>>
+    @Binding var currentValue: ClosedRange<Int>
     let sliderBounds: ClosedRange<Int>
 
+    var minAmountTextFieldViewModel: OfferAmountTextFieldViewModel
+    var maxAmountTextFieldViewModel: OfferAmountTextFieldViewModel
+
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: Appearance.GridGuide.point) {
             HStack(spacing: Appearance.GridGuide.point) {
                 Image(systemName: "plus.forwardslash.minus")
 
@@ -22,19 +25,63 @@ struct OfferAmountRangeView: View {
             }
             .foregroundColor(Appearance.Colors.whiteText)
 
+            HStack(spacing: Appearance.GridGuide.mediumPadding2) {
+                Text(L.offerCreateAmountMinTitle())
+                    .textStyle(.paragraphMedium)
+                    .foregroundColor(Appearance.Colors.gray4)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text(L.offerCreateAmountMaxTitle())
+                    .textStyle(.paragraphMedium)
+                    .foregroundColor(Appearance.Colors.gray4)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(maxWidth: .infinity)
+
+            HStack(alignment: .center, spacing: Appearance.GridGuide.point) {
+                OfferAmountTextField(
+                    viewModel: minAmountTextFieldViewModel
+                )
+
+                Text("–")
+                    .textStyle(.paragraphMedium)
+                    .foregroundColor(Appearance.Colors.gray4)
+
+                OfferAmountTextField(
+                    viewModel: maxAmountTextFieldViewModel
+                )
+            }
+
             RangePickerView(currency: currency,
-                            currentValue: currentValue,
+                            currentValue: $currentValue,
                             sliderBounds: sliderBounds)
         }
     }
 }
 
 #if DEBUG || DEVEL
+import Combine
+
 struct OfferAmountRangeViewPreview: PreviewProvider {
+
     static var previews: some View {
         OfferAmountRangeView(currency: .usd,
                              currentValue: .constant(3...8),
-                             sliderBounds: 1...10)
+                             sliderBounds: 1...10,
+                             minAmountTextFieldViewModel: .init(
+                                type: .min,
+                                currentAmountRangePublisher: Just(0...10_000).eraseToAnyPublisher(),
+                                sliderBoundsPublisher: Just(0...10_000).eraseToAnyPublisher(),
+                                currencyPublisher: Just(.usd).eraseToAnyPublisher(),
+                                rangeSetter: { _ in }
+                             ),
+                             maxAmountTextFieldViewModel: .init(
+                                type: .min,
+                                currentAmountRangePublisher: Just(0...10_000).eraseToAnyPublisher(),
+                                sliderBoundsPublisher: Just(0...10_000).eraseToAnyPublisher(),
+                                currencyPublisher: Just(.usd).eraseToAnyPublisher(),
+                                rangeSetter: { _ in }
+                             ))
             .previewDevice("iPhone 11")
             .background(Color.black)
     }
