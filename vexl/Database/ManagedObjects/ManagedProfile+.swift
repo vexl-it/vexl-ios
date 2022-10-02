a519bad66226e31b64aa1f069b8ad651f4718c61
Diego <diego.espinoza@cleevio.com>
2022-10-01 22:40:36 -0500

    created async fetch method

--- vexl/Database/ManagedObjects/ManagedProfile+.swift
+++ vexl/Database/ManagedObjects/ManagedProfile+.swift
@@ -6,12 +6,38 @@
 //
 
 import UIKit
+import Combine
 
 extension ManagedProfile {
+
+    private static let avatarQueue = DispatchQueue(label: "Avatar Image Download", qos: .userInitiated)
 
     var avatar: Data? {
         get { avatarData ?? UIImage(named: R.image.profile.avatar.name)?.pngData() }// TODO: generate random avatar
         set { avatarData = newValue }
+    }
+
+    func getAvatar() -> AnyPublisher<Data?, Never> {
+        let placeholder = UIImage(named: R.image.profile.avatar.name)?.pngData()
+
+        return Future { [weak self] promise in
+            guard let owner = self else {
+                promise(.success(placeholder))
+                return
+            }
+
+            if let avatarData = owner.avatarData {
+                promise(.success(avatarData))
+            } else if let avatarURL = owner.avatarURL, let url = URL(string: avatarURL) {
+                Self.avatarQueue.async {
+                    let data = try? Data(contentsOf: url)
+                    promise(.success(data))
+                }
+            } else {
+                promise(.success(placeholder))
+            }
+        }
+        .eraseToAnyPublisher()
     }
 
     func generateRandomName() {
