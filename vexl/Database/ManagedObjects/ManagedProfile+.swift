abf27fe9bc5d3a14ac065946d00a76bcd8ddf51d
Diego <diego.espinoza@cleevio.com>
2022-10-03 21:34:18 -0500

    added support for avatar that comes as data

--- vexl/Database/ManagedObjects/ManagedProfile+.swift
+++ vexl/Database/ManagedObjects/ManagedProfile+.swift
@@ -17,12 +17,6 @@ extension ManagedProfile {
         set { avatarData = newValue }
     }
 
-    func setAvatar(withURL url: String?) {
-        avatarURL = url
-        guard let url = url, let dataURL = URL(string: url), let data = try? Data(contentsOf: dataURL) else { return }
-        avatar = data
-    }
-
     func generateRandomName() {
         self.name = Self.generateRandomName()
     }
