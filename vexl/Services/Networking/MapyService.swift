7a53777b33e4424713e02832e8f6c7a8c62d1d8e
David Novák <kaladivo@gmail.com>
2023-03-24 14:12:03 +0100

    Remove unnecessary filtering of location results

--- vexl/Services/Networking/MapyService.swift
+++ vexl/Services/Networking/MapyService.swift
@@ -18,12 +18,6 @@ final class MapyService: BaseService, MapyServiceType {
         return request(type: MapyResponse.self, endpoint: MapyRouter.suggestions(text: text))
             .map(\.result)
             .map { $0.map { $0.userData } }
-            .map {
-                Set($0.filter { suggestion in
-                    let diacriticInsensitiveSuggestionCity = suggestion.city.folding(options: .diacriticInsensitive, locale: nil).lowercased()
-                    return !suggestion.city.isEmpty && diacriticInsensitiveSuggestionCity.lowercased().contains(diacriticInsensitiveText.lowercased())
-                })
-            }
             .map(Array.init)
             .eraseToAnyPublisher()
     }
